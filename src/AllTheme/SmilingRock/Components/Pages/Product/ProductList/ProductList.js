5e257ca536a0ee726a4498433c6561eae95eeb6b
optigo-app <130429763+optigo-app@users.noreply.github.com>
2024-06-11 13:41:07 +0530

    list and detail page routing setup

---
+++ src/AllTheme/SmilingRock/Components/Pages/Product/ProductList/ProductList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './productlist.scss'
+
+const ProductList = () => {
+  return (
+    <div id="top">
+      <div className='smr_bodyContain'>
+        <div className='smr_outerContain'>
+          <div className='smr_whiteInnerContain'></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProductList
--- src/AllTheme/SmilingRock/SmilingRock_App.js
+++ src/AllTheme/SmilingRock/SmilingRock_App.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './Components/Pages/Home/Index'
+import ProductList from './Components/Pages/Product/ProductList/ProductList'
+import ProductDetail from './Components/Pages/Product/ProductDetail/ProductDetail'
 
 const SmilingRock_App = () => {
     return (
-        <div>
+        <>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/productlist" element={<ProductList />} />
+                <Route path="/productdetail" element={<ProductDetail />} />
             </Routes>
-        </div>
+        </>
     )
 }
 
