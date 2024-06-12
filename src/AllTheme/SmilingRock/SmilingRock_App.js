f07106337a1767d4981bbe9ce95507c36deac6d4
optigo-app <130429763+optigo-app@users.noreply.github.com>
2024-06-12 13:26:52 +0530

    problem solving after git pull in code of routing cart to cartpage

--- src/AllTheme/SmilingRock/SmilingRock_App.js
+++ src/AllTheme/SmilingRock/SmilingRock_App.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './Components/Pages/Home/Index'
 import Header from './Components/Pages/Home/Header/Header'
-import Cart from './Components/Pages/Cart/Cart'
 import ProductList from './Components/Pages/Product/ProductList/ProductList'
 import ProductDetail from './Components/Pages/Product/ProductDetail/ProductDetail'
+import CartDetails from './Components/Pages/Cart/CartDetails'
 
 const SmilingRock_App = () => {
     return (
