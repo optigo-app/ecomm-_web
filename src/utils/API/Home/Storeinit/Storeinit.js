6f9268fbd255a84b062ba5a0ba04806849e219d9
jenish <jenish@gmail.com>
2024-07-01 13:50:33 +0530

    set the url api

--- src/utils/API/Home/Storeinit/Storeinit.js
+++ src/utils/API/Home/Storeinit/Storeinit.js
@@ -5,7 +5,8 @@ import axios from 'axios';
 export const Storeinit = async (param) => {
 
   // const APIURL = 'https://api.optigoapps.com/storev26/store.aspx';
-  const APIURL = 'https://api.optigoapps.com/storev26/ReactStore.aspx';
+
+  const APIURL = (window.location.hostname === 'localhost' || window.location.hostname === 'zen') ? 'http://zen/api/ReactStore.aspx' : 'https://api.optigoapps.com/storev26/ReactStore.aspx';
   // const APIURL = 'http://zen/api/ReactStore.aspx'
 
   console.log('eeeeeeeee',window.location);
