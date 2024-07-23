ad0872f9ddc728a013f4c8cc938c403bdb7a0708
jenis <jenis@gmail.com>
2024-07-23 11:28:47 +0530

    set the album mobile view

--- src/AllTheme/SmilingRock/SmilingRock_App.js
+++ src/AllTheme/SmilingRock/SmilingRock_App.js
@@ -8,7 +8,6 @@ import ContinueWithEmail from "./Components/Pages/Auth/ContinueWithEmail/Continu
 import LoginWithEmail from "./Components/Pages/Auth/LoginWithEmail/LoginWithEmail";
 import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 import { companyLogo, loginState } from "./Components/Recoil/atom";
-import { Storeinit } from "../../utils/API/Home/Storeinit/Storeinit";
 import ProductList from "./Components/Pages/Product/ProductList/ProductList";
 import ProductDetail from "./Components/Pages/Product/ProductDetail/ProductDetail";
 import ContactUs from "./Components/Pages/FooterPages/contactUs/ContactUs";
