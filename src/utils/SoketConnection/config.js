1ca2298ec16b0c8587ff02b706253d0cdc4fc2c5
jenis <jenis@gmail.com>
2024-08-08 10:55:20 +0530

    set the soket

---
+++ src/utils/SoketConnection/config.js
@@ -0,0 +1,13 @@
+const config = {
+    di: 'orail24',
+    En_ufcc: 'orail24',
+    En_UI: '15864',
+    En_UN: 'shreon prakash',
+    En_UC: 'shreon',
+    En_IMP: 'http://nzen/R50B3/UFS/ufs2/orail228FT0OWNGEI6DC3BVS//CustomerImages/',
+    address: 'http://nzen/optiogo-backend',
+    SoPath: '/connect_socket_react/socket.io',
+    iTaskCallBackURL: ''
+  };
+  
+  export default config;
