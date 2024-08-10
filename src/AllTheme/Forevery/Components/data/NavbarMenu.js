32d5e811f3b649aada485d4ad95120f298f7371c
rajan <rajan@gmail.com>
2024-08-10 14:18:17 +0530

    forevery new

--- src/AllTheme/Forevery/Components/data/NavbarMenu.js
+++ src/AllTheme/Forevery/Components/data/NavbarMenu.js
@@ -1,5 +1,23 @@
 import { GiDiamondRing } from "react-icons/gi";
 import { IoDiamondOutline } from "react-icons/io5";
+import roundImg from "../Assets/diamond/i-round.png";
+import pearImg from "../Assets/diamond/i-peer.png";
+import princessImg from "../Assets/diamond/i-princess.png";
+import asscherImg from "../Assets/diamond/i-asscher.png";
+import cushionImg from "../Assets/diamond/i-cushion.png";
+import heartImg from "../Assets/diamond/i-heart.png";
+import ovalImg from "../Assets/diamond/i-oval.png";
+import radiantImg from "../Assets/diamond/i-radiant.png";
+import emeraldImg from "../Assets/diamond/i-emerald.png";
+import marquiseImg from "../Assets/diamond/i-marquise.png";
+import ringsS from "../Assets/diamond/side/wedding-rings.png";
+import earringsS from "../Assets/diamond/side/easrrings.png";
+import pendantS from "../Assets/diamond/side/s.png";
+
+import Collection1 from "../Assets/collections/ikigai-banner.webp";
+import Collection2 from "../Assets/collections/heritage-banner.webp";
+import Collection3 from "../Assets/collections/icon-banner.webp";
+
 const Writing = (
   <img
     src="https://www.forevery.one/images_new/bespoke-icon.png"
@@ -19,21 +37,22 @@ const Mens = (
     alt=""
   />
 );
-export const NavbarMenu = [
+ const NavbarMenu = [
   {
     category: "Engagement & Wedding Diamonds",
     submenu: [
       {
         title: ` Engagement Ring`,
-        icon  : <GiDiamondRing /> ,
+        align: "col",
         submenu: [
           {
+            icon: <GiDiamondRing />,
+            icon2: <IoDiamondOutline />,
             title: "create your own diamond ring",
             menu: ["start with a setting", "Start With a Diamond"],
           },
           {
             title: `shop by style`,
-            icon  :<IoDiamondOutline /> ,
             menu: ["Solitaire", "Halo", "Vintage", "Side Stone", "Designer"],
           },
           {
@@ -44,11 +63,12 @@ export const NavbarMenu = [
       },
       {
         title: `Wedding Ring`,
-        icon : <GiDiamondRing /> ,
+        icon: <GiDiamondRing />,
         submenu: [
           {
             title: "Womens",
             icon: Women,
+            align: "row",
             menu: [
               "Classic Rings",
               "Diamond Rings",
@@ -59,10 +79,14 @@ export const NavbarMenu = [
           },
           {
             title: "Mens",
+            align: "row",
             icon: Mens,
             menu: ["Carved Rings", "Diamond Rings", "Classic Rings"],
           },
         ],
+      },
+      {
+        img: "https://www.forevery.one/images_new/foreveryimg/engagement-submenu-img.png",
       },
     ],
   },
@@ -79,3 +103,48 @@ export const NavbarMenu = [
     category: "Letter Diamonds",
   },
 ];
+
+// Creating an array of diamond objects
+const diamondShapes = [
+  { name: "Round", img: roundImg },
+  { name: "Pear", img: pearImg },
+  { name: "Princess", img: princessImg },
+  { name: "Asscher", img: asscherImg },
+  { name: "Cushion", img: cushionImg },
+  { name: "Heart", img: heartImg },
+  { name: "Oval", img: ovalImg },
+  { name: "Radiant", img: radiantImg },
+  { name: "Emerald", img: emeraldImg },
+  { name: "Marquise", img: marquiseImg },
+];
+
+
+
+const SideItems = [
+  {name  :"Diamond Rings" , img :ringsS},
+  {name  :"Diamond Earrings" , img :pendantS},
+  {name  :"Diamond Pendant" , img : earringsS}
+]
+
+// ikgai
+// IKIGAI
+// collection
+// shop the collection
+// heritage-banner
+// heritage
+// collection
+// shop the collection
+// icon-banner
+// Icon
+// collection
+// shop the collection
+// bespoke jewelry  make 
+
+
+const CollectionData = [
+  {name  :"IKIGAI" ,img : Collection1},
+  {name  :"heritage" ,img :Collection2 },
+  {name  :"Icon" ,img : Collection3},
+]
+
+export  {SideItems ,diamondShapes ,NavbarMenu ,CollectionData};
