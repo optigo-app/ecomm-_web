d8462253c6aed6d42d76890942de1d1b8522cb9b
optigo-app <130429763+optigo-app@users.noreply.github.com>
2024-08-13 10:25:00 +0530

    diamondtine cart and wislist bugs

--- src/utils/API/CartAPI/QuantityAPI.js
+++ src/utils/API/CartAPI/QuantityAPI.js
@@ -1,10 +1,11 @@
 import { CommonAPI } from "../CommonAPI/CommonAPI";
 
-export const updateQuantity = async (num, lastEnteredQuantity, visiterId, islogin) => {
+export const updateQuantity = async (num, lastEnteredQuantity, visiterId) => {
   try {
     const storeInit = JSON.parse(sessionStorage.getItem("storeInit"));
     const { FrontEnd_RegNo } = storeInit;
     const storedData = sessionStorage.getItem("loginUserDetail");
+    const islogin = JSON.parse(sessionStorage.getItem("LoginUser"));
     const data = JSON.parse(storedData);
     const customerId = storeInit?.IsB2BWebsite == 0 && islogin == false || islogin == null  ? visiterId : data?.id ?? 0;
     const customerEmail = storeInit?.IsB2BWebsite == 0 && islogin == false || islogin == null  ? visiterId : data?.userid ?? "";
