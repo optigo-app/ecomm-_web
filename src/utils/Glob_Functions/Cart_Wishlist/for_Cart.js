088576be95721e819c418de2f4f8bcd034a8662d
optigo-app <130429763+optigo-app@users.noreply.github.com>
2024-08-30 14:16:52 +0530

    forevery diamond list filter

--- src/utils/Glob_Functions/Cart_Wishlist/for_Cart.js
+++ src/utils/Glob_Functions/Cart_Wishlist/for_Cart.js
@@ -232,8 +232,9 @@ const useCart = () => {
         let param = "Cart";
         let cartfilter = cartData?.filter(cartItem => cartItem.id !== item.id);
         setCartData(cartfilter);
-        let diaFilter = diamondCartData?.filter(dia => dia?.stockno !== item?.stockno);
+        let diaFilter = diamondCartData?.filter(dia => dia?.stockno !== item?.Sol_StockNo);
         setDiamondCartData(diaFilter);
+
 
         setTimeout(() => {
             if (cartfilter && isMaxWidth1050) {
