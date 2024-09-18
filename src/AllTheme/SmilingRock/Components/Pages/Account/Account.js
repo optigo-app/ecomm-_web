64710b852f388c7799d2472521bb4e0e9fff9c45
User <optigoapi@gmail.com>
2024-09-18 14:18:50 +0530

    plm flag wise set to all themes

--- src/AllTheme/SmilingRock/Components/Pages/Account/Account.js
+++ src/AllTheme/SmilingRock/Components/Pages/Account/Account.js
@@ -230,11 +230,11 @@ export default function Account() {
                                     <ChangePassword />
                                 </div>
                             </CustomTabPanel>
-                            <CustomTabPanel value={value} index={5}>
+                            { loginUSerDeatil?.IsPLWOn && <CustomTabPanel value={value} index={5}>
                                 <div>
                                     <Plm />
                                 </div>
-                            </CustomTabPanel>
+                            </CustomTabPanel>}
 
 
                         </Box>
