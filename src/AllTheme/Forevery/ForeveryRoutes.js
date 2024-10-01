fedd59b6c9f49804010142bb7a65d09ddbdef254
optigo-app <130429763+optigo-app@users.noreply.github.com>
2024-10-01 15:23:50 +0530

    issue solving

--- src/AllTheme/Forevery/ForeveryRoutes.js
+++ src/AllTheme/Forevery/ForeveryRoutes.js
@@ -67,6 +67,7 @@ import LabCreatedRings from "./Components/Pages/labCreated-rings/LabCreatedRings
 import JewelryInquiryForm from "../../utils/Inquary/JewelryInquiryForm";
 import LabGrownWeddingRing from "./Components/Pages/lab-grown-wedding-rings/LabGrownWeddingRing";
 import ZaraStyleSlider from "./Components/Pages/AZara";
+import { storImagePath } from "../../utils/Glob_Functions/GlobalFunction";
 
 const ForEveryRoutes = () => {
   const islogin = useRecoilValue(for_loginState);
