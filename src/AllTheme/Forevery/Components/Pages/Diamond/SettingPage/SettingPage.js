9b52349067725f3771c80e6e556f8fc68ab54951
optigo-app <130429763+optigo-app@users.noreply.github.com>
2024-10-05 15:01:47 +0530

    working on forevery website

--- src/AllTheme/Forevery/Components/Pages/Diamond/SettingPage/SettingPage.js
+++ src/AllTheme/Forevery/Components/Pages/Diamond/SettingPage/SettingPage.js
@@ -22,8 +22,9 @@ import ProductListApi from '../../../../../../utils/API/ProductListAPI/ProductLi
 import { FilterListAPI } from '../../../../../../utils/API/FilterAPI/FilterListAPI';
 import { BsHandbag } from 'react-icons/bs';
 import Pako from 'pako';
-import { for_customizationSteps } from '../../../Recoil/atom';
+import { for_customizationSteps, for_MakeMyRingProcessDrawer } from '../../../Recoil/atom';
 import { useRecoilState } from 'recoil';
+import MakeRingProcessModal from '../../ReusableComponent/DiamondStepModal/MakeRingProcessModal';
 
 const SettingPage = () => {
 
@@ -36,12 +37,24 @@ const SettingPage = () => {
   let cookie = Cookies.get("visiterId");
   const dropdownRefs = useRef({})
   const [currPage, setCurrPage] = useState(1);
+  const [modalOpen, setModalOpen] = useRecoilState(for_MakeMyRingProcessDrawer);
 
   useEffect(() => {
     const aa1234 = JSON.parse(sessionStorage.getItem('custStepData'));
   }, [])
 
   const encodeLink = (link) => btoa(link);
+
+  useEffect(() => {
+    const showModal = localStorage.getItem('dontShowModal');
+    if (showModal !== 'true') {
+      setModalOpen(true);
+    }
+  }, []);
+
+  const handleButtonClick = () => {
+    setModalOpen(true);
+  };
 
   const styleLinks = {
     Solitaire: "Solitaire/style",
@@ -762,6 +775,9 @@ const SettingPage = () => {
               }
             </div>
           </div>
+          <div class="mr_Modal-imageButton" onClick={handleButtonClick}>
+            <button>How it works</button>
+          </div>
           <div className="for_settingList_product_lists_div">
             {isOnlySettLoading ? <div className="for_global_spinner"></div> : (
               productListData?.map((item, index) => (
@@ -806,6 +822,7 @@ const SettingPage = () => {
             )}
         </div>
       </div >
+      {modalOpen && <MakeRingProcessModal/>}
     </>
   )
 }
