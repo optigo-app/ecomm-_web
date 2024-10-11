c14094397cb4675d92e46aabc6a229e49b7342f0
rajan <rajan@gmail.com>
2024-10-11 17:23:58 +0530

    new changes

--- src/AllTheme/StamFordJewels/Components/Pages/Home/LookBook/Lookbook.js
+++ src/AllTheme/StamFordJewels/Components/Pages/Home/LookBook/Lookbook.js
@@ -1103,7 +1103,7 @@ const Lookbook = () => {
             </div>
           </div>
           <div className="stam_SubDiv_LookBookSubMainDiv">
-            <div className="stam_lookbookFilterMain" style={{ transition: "1s ease", backgroundColor: 'white', zIndex: '1111111111111', width: `19%`, left: `${isShowfilter ? "0" : "-500%"}`, position: 'absolute', top: '100px', display: isShowfilter ? "block" : "none" }}>
+            <div className="stam_lookbookFilterMain" style={{ transition: "1s ease", backgroundColor: 'white', zIndex: '999', width: `19%`, left: `${isShowfilter ? "0" : "-500%"}`, position: 'absolute', top: '100px', display: isShowfilter ? "block" : "none" }}>
 
               {filterData?.length > 0 && (
                 <div className="smr1_lookBookFilterSubDiv">
